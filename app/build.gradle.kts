5e6eecbc75051c7f33f78beab5a065cb56c7d82f
to-higashiguchi <to-higashiguchi@kddi-agdc.com>
2025-11-26 11:15:11 +0900

    Add Hilt and KSP dependencies to build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp) // 🔥 kotlin-kapt ではなく ksp を使います
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -54,4 +56,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler) // 🔥 kapt ではなく ksp を使います
 }
